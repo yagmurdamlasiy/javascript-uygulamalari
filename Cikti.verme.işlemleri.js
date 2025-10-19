40d584f518b4b12d8541daeee8aaafda282f629e
Yağmur Doğan <yagmur.dogan1@ogr.gelisim.edu.tr>
2025-10-19 22:42:36 +0300

    Add files via upload

---
+++ Cikti.verme.işlemleri.js
@@ -0,0 +1,15 @@
+console.log("YAĞMUR DOĞAN");
+console.log(15);
+console.log(3.14);
+console.log(true);
+
+console.dir(window);
+console.dir(window.document.location.host);
+console.dir(window.document.documentURI);
+console.dir(window.location.port);
+
+document.writeln("YAĞMUR DOĞAN" + "<br>");
+document.writeln(15 + "<br>");
+document.writeln(3.14 + "<br>");
+document.writeln(true + "<br>");
+Document.WRİTELN(null + "<br>");
