3d2215c61c6173ba935662dea1b7e13b429e03e3
Yağmur Doğan <yagmur.dogan1@ogr.gelisim.edu.tr>
2025-10-19 22:18:34 +0300

    Add files via upload

---
+++ diyalog.kutuları.js
@@ -0,0 +1,7 @@
+//alert("MERHABA YAĞMUR");
+/*let isim=prompt("Adınızı Giriniz:");
+let soyad=prompt("Soyadınızı Giriniz:");
+console.log("Adınız:"+ isim );
+console.log("Soyadınız:"+ soyad);*/
+//let sonuc=confirm("SİLMEK İSTEDİĞİNİZE EMİN MİSİNİZ?");//
+swal.fire("MERHABA YAĞMUR");
