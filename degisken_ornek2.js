848821b1eaa46785ee89e8082a1ddd4a17042e27
Yağmur Doğan <yagmur.dogan1@ogr.gelisim.edu.tr>
2025-10-19 20:00:56 +0300

    Add files via upload

---
+++ degisken_ornek2.js
@@ -0,0 +1,17 @@
+var ad="EFE";
+console.log(ad);
+let okul="";
+console.log(okul);
+okul="Gökkuşağı Koleji";
+console.log(okul);
+let program="Sayısal";
+console.log(program);
+program="Sözel";
+console.log(program);
+let sifre="deneme";
+console.log(sifre);
+document.writeln("<h3>Kayıt Ekranı</h3>"+"<br>"+
+"Adı: "+ad+"<br>"+
+"Okul İsmi: "+okul+"<br>"+
+"Program Bilgisi: "+program+"<br>"+
+"Şifre Bilgisi: "+sifre);
