db3dc04b17526e03f82da1780175a463067f591a
Yağmur Doğan <yagmur.dogan1@ogr.gelisim.edu.tr>
2025-10-19 21:18:49 +0300

    Add files via upload

---
+++ Degisken.aritmetik.js
@@ -0,0 +1,22 @@
+let sayi1=17;
+let sayi2=15;
+let toplam=sayi1+sayi2;
+let cikar=sayi1-sayi2;
+let carp=sayi1*sayi2;
+let bol=sayi1/sayi2;
+let mod=sayi1%sayi2;
+let us=sayi1**sayi2;
+document.wiretln(
+    "<h3>Basit Hesap Makinesi</h3><br>",
+    "<b>Birinci Sayı:17<br> İkinci Sayı:15<br></b>",
+    "Toplama İşlemi Sonucu: ",topla,"<br>",
+    "Çıkarma İşlemi Sonucu: ",cikar,"<br>",
+    "Çarpma İşlemi Sonucu: ",carp,"<br>",
+    "Bölme İşlemi Sonucu: ",bol,"<br>",
+    "Mod Alma İşlemi Sonucu: ",mod,"<br>",
+    "Üs Alma İşlemi Sonucu: ",usAlma, "<br>",
+    "Aşağı Yuvarlama İşlemi Sonucu: ",Math.floor(sayi1),"<br>",
+    "Yukarı Yuvarlama İşlemi Sonucu: ",Math.ceil(sayi1),"<br>",
+    "Yakında Yuvarlama İşlemi Sonucu: ",Math.round(sayi1),
+
+);
