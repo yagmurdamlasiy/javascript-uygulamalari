cb3a454e021885900f91462655159057a847a4de
Yağmur Doğan <yagmur.dogan1@ogr.gelisim.edu.tr>
2025-10-19 22:32:22 +0300

    Add files via upload

---
+++ Degisken.tanimlama.js
@@ -0,0 +1,23 @@
+var ad="yağmur";
+console.log(ad);
+
+let okul;
+console.log(okul);
+
+okul="GELİŞİM ÜNİVERSİTESİ";
+console.log(okul);
+
+let program="ÖN YÜZ YAZILIM GELİŞTİRME";
+console.log(program);
+
+program="JAVASCRİPT UYGULAMALARI";
+console.log(program);
+
+program="DERS BİLGİSİ "+ program;
+console.log(program);
+
+let sifre="1234";
+console.log(sifre);
+
+const PI=3.14;
+console.log(PI);
