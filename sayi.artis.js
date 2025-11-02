bf3b28c222e02806fe83d2f34b67b50311bc00fb
Yağmur Doğan <yagmur.dogan1@ogr.gelisim.edu.tr>
2025-11-03 00:32:04 +0300

    Add files via upload

---
+++ sayi.artis.js
@@ -0,0 +1,12 @@
+document.writeln("<u>0'dan 10'a kadar olan çift sayılar:</u>"+"<br>");
+for(let i=0;i<=10;i+=2)
+{
+    document.writeln(i);
+
+}
+{
+    document.writeln("<br><u>1'den 10'a kadar olan tek sayılar:</u>"+"<br>")
+    for(let j=1;j<=10;j+=2)
+        document.writeln(j);
+    
+}
