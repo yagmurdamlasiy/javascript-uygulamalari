f6527ed9fcaf8045e6141d0dcba1bd4bd2630c4a
Yağmur Doğan <yagmur.dogan1@ogr.gelisim.edu.tr>
2025-11-03 00:31:47 +0300

    Add files via upload

---
+++ tahmin.uyg.js
@@ -0,0 +1,22 @@
+let sayi=prompt("1 ile 5 arasında bir sayı giriniz:");
+switch(sayi)
+{
+    case "1":
+        alert("Girilen sayı 1'dir.");
+        break;
+    case "2":
+        alert("Girilen sayı 2'dir.");
+        break;
+    case "3":
+        alert("Girilen sayı 3'dir.");
+        break;
+    case "4":
+        alert("Girilen sayı 4'tür.");
+        break;
+    case "5":
+        alert("Girilen sayı 5'tir.");
+        break;
+    default:
+        alert("Geçersiz sayı girişi yapılmıştır.");
+        break;
+}
